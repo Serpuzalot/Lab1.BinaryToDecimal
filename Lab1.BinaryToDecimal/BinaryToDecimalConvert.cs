5579a0824159889d7abb4518b0ea7315d96745c6
Konstantin Borzik <kosty5192@gmail.com>
2021-09-24 00:54:56 +0300

    Ready converter binary to decimal converter

--- Lab1.BinaryToDecimal/BinaryToDecimalConvert.cs
+++ Lab1.BinaryToDecimal/BinaryToDecimalConvert.cs
@@ -11,31 +11,10 @@ namespace Lab1.BinaryToDecimal
         private List<int> _numList;
         private int _dotPosition = int.MinValue;
 
-        public BinaryToDecimalConvert(double num)
-        {
-            if (!CheckEnterData(num))
-            {
-                return;
-            }
-            string numStr = num.ToString();
-
-            _numList = new List<int>();
-            for (int i = 0; i < numStr.Length; i++)
-            {
-                if (i != _dotPosition)
-                {
-                    _numList.Add(Convert.ToInt32(numStr[i].ToString()));
-                }
-                
-            }
-
-
-        }
-
         bool CheckEnterData(double num)
         {
             string str = num.ToString();
-            if (dotCheck(str) == true && numCheck(str) == true)
+            if (DotCheck(str) == true && NumCheck(str) == true)
             {
                 if (num != null && num >= 0)
                 {
@@ -45,7 +24,7 @@ namespace Lab1.BinaryToDecimal
             return false;
         }
 
-        private bool dotCheck(string str)
+        private bool DotCheck(string str)
         {
             for (int i = 0; i < str.Length; i++)
             {
@@ -62,7 +41,7 @@ namespace Lab1.BinaryToDecimal
             return true;
         }
 
-        private bool numCheck(string str)
+        private bool NumCheck(string str)
         {
             for (int i = 0; i < str.Length; i++)
             {
@@ -78,6 +57,68 @@ namespace Lab1.BinaryToDecimal
 
             }
             return true;
+        }
+
+        private double GetResultBehindDot()
+        {
+            double result = 0;
+            int start;
+            if (_dotPosition == int.MinValue)
+            {
+                start = _numList.Count - 1;
+            }
+            else
+            {
+                start = _dotPosition - 1;
+            }
+            for (int i = start; i >= 0; i--)
+            {
+                if (_numList[i] == 1)
+                {
+                    result += 1 * Math.Pow(2, (start) - i);
+                }
+            }
+            return result;
+        }
+
+        private double GetResultAfterDot()
+        {
+            double result = 0;
+            int counter = 1;
+            for(int i = _dotPosition ; i < _numList.Count; i++)
+            {
+                if(_numList[i] == 1)
+                {
+                    result += 1.0 * Math.Pow(2, -counter);
+                }
+                counter++;
+            }
+            return result;
+        }
+
+        public double GetResult(double num)
+        {
+            if (!CheckEnterData(num))
+            {
+                return double.NaN;
+            }
+            string numStr = num.ToString();
+            _numList = new List<int>();
+            for (int i = 0; i < numStr.Length; i++)
+            {
+                if (i != _dotPosition)
+                {
+                    _numList.Add(Convert.ToInt32(numStr[i].ToString()));
+                }
+
+            }
+            double result = 0;
+            result += GetResultBehindDot();
+            if(_dotPosition != int.MinValue)
+            {
+                result += GetResultAfterDot();
+            }
+            return result;
         }
 
     }
--- Lab1.BinaryToDecimal/Program.cs
+++ Lab1.BinaryToDecimal/Program.cs
@@ -8,10 +8,80 @@ namespace Lab1.BinaryToDecimal
 {
     class Program
     {
+        static int Menu()
+        {
+            Console.WriteLine("Chose mode:\n1.Convert ready examples\n2.Enter the number manually\n3.Exit");
+            int choice;
+            bool result = int.TryParse(Console.ReadLine(), out choice);
+            while(result == false || choice <0 || choice > 4)
+            {
+                Console.Clear();
+                Console.WriteLine("Operation input error,try again");
+                Console.WriteLine("Chose mode:\n1.Convert ready examples\n2.Enter the number manually\n3.Exit");
+                result = int.TryParse(Console.ReadLine(), out choice);
+            }
+            return choice;
+        }
+
+        static List<ulong> GetExamples()
+        {
+            List<ulong> examples = new List<ulong>();
+            Random rand = new Random();
+            string num = "";
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    if ( j == 0 || j == 12)
+                    {
+                        num += "1";
+                        continue;
+                    }
+                    num += rand.Next(0, 2).ToString();
+                }
+                examples.Add(ulong.Parse(num));
+                num = "";
+            }
+            return examples;
+            
+        }
+
         static void Main(string[] args)
         {
-            BinaryToDecimalConvert toDecimalConvert = new BinaryToDecimalConvert(01011);
-            Console.ReadKey();
+            BinaryToDecimalConvert binaryToDecimal = new BinaryToDecimalConvert();
+            bool stopButton = false;
+            while (!stopButton)
+            {
+                switch (Menu())
+                {
+                    case 1:
+                        List<ulong> list = GetExamples();
+                        foreach(var el in list)
+                        {
+                            Console.WriteLine(el + " => " + binaryToDecimal.GetResult(el));
+                        }
+
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Enter your binary number:");
+                        double num;
+                        bool convertCheck = double.TryParse(Console.ReadLine(), out num);
+                        while (!convertCheck)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Input error,try again");
+                            Console.WriteLine("Enter your binary number:");
+                            convertCheck = double.TryParse(Console.ReadLine(), out num);
+                        }
+                        Console.WriteLine("Result: " + num + " => " + binaryToDecimal.GetResult(num));
+                        break;
+                    case 3:
+                        stopButton = true;
+                        break;
+                }
+            }
+            
         }
     }
 }
